f7c15e7e67d5cf46bfbbe99320edfb155efa0985
mikah13 <anhminhhoang13@gmail.com>
2023-11-18 23:55:56 -0800

    update site config and metadata

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Mike Hoang',
+	title: 'Mike Hoang | Personal website',
+	description:
+		'I am a software developer with a passion for creating innovative web applications. Well-versed in React.js and TypeScript, I always strive to make the complex seem effortless.',
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
