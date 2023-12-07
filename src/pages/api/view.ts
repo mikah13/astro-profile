6e3e4b8b82ab0352cb234d71d1d6396fd6dd3690
mikah13 <anhminhhoang13@gmail.com>
2023-12-07 03:41:03 -0800

    update: add snippet blog, prettierrc rule

--- src/pages/api/view.ts
+++ src/pages/api/view.ts
@@ -3,13 +3,22 @@ import { supabase } from 'src/utils/db'
 
 export const GET: APIRoute = async ({ params, request }) => {
 	try {
-		const runPageView = await supabase.rpc('increment_views', { page_slug: req.body.slug })
+		console.log({ params })
+
+		let { data, error } = await supabase
+			.from('pageview')
+			.select()
+			.eq('pageview.pageid', 'test')
+
+		console.log(data)
+		if (error) return new Response(JSON.stringify(error))
+		else return new Response(JSON.stringify(data))
 	} catch (error) {}
 
 	return new Response(
 		JSON.stringify({
 			name: 'Astro',
-			url: 'https://astro.build/'
+			url: 'https://astro.build/',
 		})
 	)
 }
